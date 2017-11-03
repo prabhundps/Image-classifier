1e6838ac4087452fbb6bb44dffb0d41b176f1661
prabhundps <prabhundps@gmail.com>
2017-11-03 21:05:26 +0530

    Update knn.py

--- knn.py
+++ knn.py
@@ -6,10 +6,85 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
-from pyimagesearch.preprocessing import SimplePreprocessor
-from pyimagesearch.datasets import SimpleDatasetLoader
-from imutils import paths
 import argparse
+import cv2
+
+
+class SimplePreprocessor:
+	def __init__(self, width, height, inter=cv2.INTER_AREA):
+		# store the target image width, height, and interpolation
+		# method used when resizing
+		self.width = width
+		self.height = height
+		self.inter = inter
+
+	def preprocess(self, image):
+		# resize the image to a fixed size, ignoring the aspect
+		# ratio
+		return cv2.resize(image, (self.width, self.height),
+			interpolation=self.inter)
+class paths:
+	def list_images(basePath, contains=None):
+		return list_files(basePath, validExts=(".jpg", ".jpeg", ".png", ".bmp"), contains=contains)
+
+	def list_files(basePath, validExts=(".jpg", ".jpeg", ".png", ".bmp"), contains=None):
+		# loop over the directory structure
+		for (rootDir, dirNames, filenames) in os.walk(basePath):
+            # loop over the filenames in the current directory
+			for filename in filenames:
+				# if the contains string is not none and the filename does not contain
+            # the supplied string, then ignore the file
+				if contains is not None and filename.find(contains) == -1:
+					continue
+					# determine the file extension of the current file
+				ext = filename[filename.rfind("."):].lower()
+				if ext.endswith(validExts):
+					imagePath = os.path.join(rootDir, filename).replace(" ", "\\ ")
+					yield imagePath
+
+					
+class SimpleDatasetLoader:
+	def __init__(self, preprocessors=None):
+		# store the image preprocessor
+		self.preprocessors = preprocessors
+
+		# if the preprocessors are None, initialize them as an
+		# empty list
+		if self.preprocessors is None:
+			self.preprocessors = []
+
+	def load(self, imagePaths, verbose=-1):
+		# initialize the list of features and labels
+		data = []
+		labels = []
+
+		# loop over the input images
+		for (i, imagePath) in enumerate(imagePaths):
+			# load the image and extract the class label assuming
+			# that our path has the following format:
+			# /path/to/dataset/{class}/{image}.jpg
+			image = cv2.imread(imagePath)
+			label = imagePath.split(os.path.sep)[-2]
+
+			# check to see if our preprocessors are not None
+			if self.preprocessors is not None:
+				# loop over the preprocessors and apply each to
+				# the image
+				for p in self.preprocessors:
+					image = p.preprocess(image)
+
+			# treat our processed image as a "feature vector"
+			# by updating the data list followed by the labels
+			data.append(image)
+			labels.append(label)
+
+			# show an update every `verbose` images
+			if verbose > 0 and i > 0 and (i + 1) % verbose == 0:
+				print("[INFO] processed {}/{}".format(i + 1,
+					len(imagePaths)))
+
+		# return a tuple of the data and labels
+		return (np.array(data), np.array(labels))
 
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
